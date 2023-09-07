2c6af37b436667a54d2835cf4c2aeea4bb69defc
lanxeyu <lanxeyu@gmail.com>
2023-09-07 12:27:40 +0100

    Add question and powerstat array

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const Question = () => {
-  // get random attribute from a list
-  // display question with attribute
+const Question = ({ powerstatsArray }) => {
+  const [question, setQuestion] = useState('');
+
+  useEffect(() => {
+    // Get random powerstat 
+    const randomPowerstat = powerstatsArray[Math.floor(Math.random() * powerstatsArray.length)];
+
+    // Create question based on random powerstat
+    const questionText = `Which hero has a higher ${randomPowerstat}?`;
+    setQuestion(questionText);
+  }, []);
+
   return (
-    <div>Question</div>
-  )
-}
+    <div>
+      <h1>{question}</h1>
+    </div>
+  );
+};
 
-export default Question
+export default Question;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { HeroCards, Question, Score } from '../../components'
 
 const Home = () => {
+
+  const powerstatsArray = ["intelligence", "strength", "speed", "durability", "power", "combat"];
+
   // attribute, list of cards and that attribute
   return (
     <>
-      <Question />
+      <Question powerstatsArray={powerstatsArray} />
       <HeroCards />
       <Score />
     </>
